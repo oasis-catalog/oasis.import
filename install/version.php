cd89d6479199cf20318f5261e329419cc325b61d
Nikola <nikolaapolsid@gmail.com>
2025-07-02 19:08:44 +0300

    fix version

--- install/version.php
+++ install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = [
-    'VERSION'      => '2.0.1',
-    'VERSION_DATE' => '2025-05-22 00:00:00'
+    'VERSION'      => '2.0.2',
+    'VERSION_DATE' => '2025-07-02 00:00:00'
 ];
